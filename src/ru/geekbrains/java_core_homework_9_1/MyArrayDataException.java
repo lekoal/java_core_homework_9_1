8368e7785506aa4337f16d1ad16abcf828c41f44
lekoal <coreglus@gmail.com>
2021-09-03 12:04:27 +0300

    Task is completed

--- src/ru/geekbrains/java_core_homework_9_1/MyArrayDataException.java
+++ src/ru/geekbrains/java_core_homework_9_1/MyArrayDataException.java
@@ -1,6 +1,6 @@
 package ru.geekbrains.java_core_homework_9_1;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends Exception {
     public MyArrayDataException(int x, int y) {
         System.out.printf("Cant convert symbol to int. Error in cell: %s : %s%n", x, y);
     }
